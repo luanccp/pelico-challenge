cb5cc74d2bf652a6cc19b18daaa1632156d51723
Luan Castheloge <luanccp@gmail.com>
2023-08-05 16:12:36 +0100

    feat: remove favorite repo

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,10 +1,46 @@
+import { useContext } from "react";
 import { SearchAppBar } from "../../components/SearchAppBar";
+import { FavoriteContext } from "../../context/Favorite";
+import {
+  ResultCard,
+  ResultCardContent,
+} from "../../components/SearchAppBar/styles";
+import { Button, Typography } from "@mui/material";
+import { EmptyContainer } from "./styles";
 
-export function Favorites() {
+export const Favorites: React.FC = () => {
+  const { favoriteList, onRemoveFavorite } = useContext(FavoriteContext);
+
+  if (!favoriteList.length) {
+    return (
+      <div>
+        <SearchAppBar />
+        <EmptyContainer>
+          <Typography variant="h6">No favorites here.</Typography>
+          <Typography variant="body1">
+            Please go back at home and fav any repo you like :)
+          </Typography>
+        </EmptyContainer>
+      </div>
+    );
+  }
+
   return (
     <div>
       <SearchAppBar />
       <h2> Favorites</h2>
+      {favoriteList.map((repo) => (
+        <ResultCard key={repo.id} square>
+          <ResultCardContent>
+            <Typography variant="h6" gutterBottom>
+              {repo.name}
+            </Typography>
+          </ResultCardContent>
+          <Button size="small" onClick={() => onRemoveFavorite(repo.id)}>
+            Remove Favorite
+          </Button>
+        </ResultCard>
+      ))}
     </div>
   );
-}
+};
